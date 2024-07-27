8f549a5baaf0aceb522d0b2d45a28adb0db5544e
DJ Doena <djdoena@outlook.com>
2024-07-27 18:14:51 +0200

    CalculateDvdDiscId 1.0.5

--- CalculateDvdDiscIdLibrary/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Calculate DVD Disc Id Library")]
-[assembly: AssemblyCompany("Doena Soft.")]
-
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
-
-[assembly: AssemblyDelaySign(false)]
